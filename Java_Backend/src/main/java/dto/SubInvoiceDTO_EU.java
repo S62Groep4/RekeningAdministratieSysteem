4b26ccd5919df8f7ed78074ffb07b2a5d5b41ac7
teunvdwijst <teunvdwijst@gmail.com>
2018-04-10 15:56:10 +0200

    init of EU DTO objects

---
+++ Java_Backend/src/main/java/dto/SubInvoiceDTO_EU.java
@@ -0,0 +1,68 @@
+package dto;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Teun
+ */
+public class SubInvoiceDTO_EU implements Serializable {
+
+    private String invoiceNumber;
+    private String country;
+    private boolean isPayed;
+    private String invoiceDate;
+    private double price;
+
+    public SubInvoiceDTO_EU() {
+    }
+
+    public SubInvoiceDTO_EU(String invoiceNumber, String country, boolean isPayed, String invoiceDate, double price) {
+        this.invoiceNumber = invoiceNumber;
+        this.country = country;
+        this.isPayed = isPayed;
+        this.invoiceDate = invoiceDate;
+        this.price = price;
+    }
+
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public boolean isIsPayed() {
+        return isPayed;
+    }
+
+    public void setIsPayed(boolean isPayed) {
+        this.isPayed = isPayed;
+    }
+
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(String invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
---
+++ Java_Backend/src/main/java/dto/VehicleDTO_EU.java
@@ -0,0 +1,69 @@
+package dto;
+
+import java.io.Serializable;
+import java.util.Base64;
+
+/**
+ *
+ * @author Teun
+ */
+public class VehicleDTO_EU implements Serializable {
+
+    private String hashedLicencePlate;
+    private String journeyUrl;
+    private int journeys;
+    private String subInvoiceUrl;
+    private int subInvoices;
+
+    public VehicleDTO_EU() {
+    }
+
+    public VehicleDTO_EU(String hashedLicencePlate, String journeyUrl, int journeys, String subInvoiceUrl, int subInvoices) {
+        this.hashedLicencePlate = new String(Base64.getEncoder().encode(hashedLicencePlate.getBytes()));
+        this.journeyUrl = journeyUrl;
+        this.journeys = journeys;
+        this.subInvoiceUrl = subInvoiceUrl;
+        this.subInvoices = subInvoices;
+    }
+
+    public String getHashedLicencePlate() {
+        return hashedLicencePlate;
+    }
+
+    public void setHashedLicencePlate(String hashedLicencePlate) {
+        this.hashedLicencePlate = hashedLicencePlate;
+    }
+
+    public String getJourneyUrl() {
+        return journeyUrl;
+    }
+
+    public void setJourneyUrl(String journeyUrl) {
+        this.journeyUrl = journeyUrl;
+    }
+
+    public int getJourneys() {
+        return journeys;
+    }
+
+    public void setJourneys(int journeys) {
+        this.journeys = journeys;
+    }
+
+    public String getSubInvoiceUrl() {
+        return subInvoiceUrl;
+    }
+
+    public void setSubInvoiceUrl(String subInvoiceUrl) {
+        this.subInvoiceUrl = subInvoiceUrl;
+    }
+
+    public int getSubInvoices() {
+        return subInvoices;
+    }
+
+    public void setSubInvoices(int subInvoices) {
+        this.subInvoices = subInvoices;
+    }
+
+}
